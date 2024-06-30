80d24094aa4ec11d0c22fd199339ab7cab8e47fe
Mohamed <aboelkassem.me@gmail.com>
2024-06-30 07:16:06 +0300

    feat: add auto mapper and mediator queries

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/Queries/GetEventDetail/GetEventsDetailQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail
+{
+    public class GetEventsDetailQueryHandler : IRequestHandler<GetEventDetailQuery, EventDetailVm>
+    {
+        private readonly IEventRepository _eventRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventsDetailQueryHandler(IEventRepository eventRepository, ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<EventDetailVm> Handle(GetEventDetailQuery request, CancellationToken cancellationToken)
+        {
+            var @event = await _eventRepository.GetByIdAsync(request.Id);
+            var eventDetailDto = _mapper.Map<EventDetailVm>(@event);
+
+            var category = await _categoryRepository.GetByIdAsync(@event.CategoryId);
+
+            eventDetailDto.Category = _mapper.Map<CategoryDto>(category);
+
+            return eventDetailDto;
+        }
+    }
+}
