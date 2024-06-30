0e975e55af08f3d2d3a7a5ce10b6efa0dd9d651e
Mohamed <aboelkassem.me@gmail.com>
2024-06-30 09:14:47 +0300

    feat: finish init core application logic

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList
+{
+    public class GetCategoriesListQueryHandler : IRequestHandler<GetCategoriesListQuery, List<CategoryListItemVm>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoriesListQueryHandler(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategoryListItemVm>> Handle(GetCategoriesListQuery request, CancellationToken cancellationToken)
+        {
+            var allCategories = (await _categoryRepository.ListAllAsync()).OrderBy(x => x.Name);
+            return _mapper.Map<List<CategoryListItemVm>>(allCategories);
+        }
+    }
+}
